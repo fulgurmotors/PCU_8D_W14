662f5e871406ebd4d32fe5be14df5ab0a5ae5370
Gauthier BARON <gauthier.baron@icloud.com>
2024-03-24 15:18:35 +0100

    Write pixel coordonates as constants

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -31,7 +31,13 @@ void drawInitLogo(){
 }
 */
 
-//Clear the screen and start a new display list
+const int top_vertical_y = 24;
+const int side_horizontal_x = 22;
+const int middle_vertical_x = 186;
+const int middle_top_horizontal_y = 61 + top_vertical_y;
+const int middle_bottom_horizontal_y = EVE_VSIZE - 61;
+const int middle_horizontal_space_y = 36;
+
 void initRefreshDisplay(){
     EVE_start_cmd_burst();
     EVE_cmd_dl_burst(CMD_DLSTART); // Instruct the co-processor to start a new display list
@@ -48,52 +54,52 @@ void drawMainCommon(){
     EVE_cmd_dl_burst( DL_BEGIN | EVE_LINES );
 
     EVE_cmd_dl_burst( LINE_WIDTH(1U*16U) );
-    EVE_cmd_dl_burst( COLOR_RGB(255, 255, 255) );
+    EVE_cmd_dl_burst( WHITE );
 
     //Top horizontal line
-    EVE_cmd_dl_burst( VERTEX2F(0, 24) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE, 24) );
+    EVE_cmd_dl_burst( VERTEX2F(0, top_vertical_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE, top_vertical_y) );
 
     //Side vertical lines
-    EVE_cmd_dl_burst( VERTEX2F(22, 24) );
-    EVE_cmd_dl_burst( VERTEX2F(22, EVE_VSIZE) );
+    EVE_cmd_dl_burst( VERTEX2F(side_horizontal_x, top_vertical_y) );
+    EVE_cmd_dl_burst( VERTEX2F(side_horizontal_x, EVE_VSIZE) );
 
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 22, 24) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 22, EVE_VSIZE) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - side_horizontal_x, top_vertical_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - side_horizontal_x, EVE_VSIZE) );
 
     //Middle vertical lines
-    EVE_cmd_dl_burst( VERTEX2F(186, 24) );
-    EVE_cmd_dl_burst( VERTEX2F(186, EVE_VSIZE) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, top_vertical_y) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, EVE_VSIZE) );
 
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, 24) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, EVE_VSIZE) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, top_vertical_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, EVE_VSIZE) );
 
     //Middle top horizontal line
-    EVE_cmd_dl_burst( VERTEX2F(22, 85) );
-    EVE_cmd_dl_burst( VERTEX2F(186, 85) );
+    EVE_cmd_dl_burst( VERTEX2F(side_horizontal_x, middle_top_horizontal_y) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, middle_top_horizontal_y) );
 
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 22, 85) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, 85) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - side_horizontal_x, middle_top_horizontal_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, middle_top_horizontal_y) );
 
     //Middle bottom horizontal line
-    EVE_cmd_dl_burst( VERTEX2F(22, EVE_VSIZE - 61) );
-    EVE_cmd_dl_burst( VERTEX2F(186, EVE_VSIZE - 61) );
+    EVE_cmd_dl_burst( VERTEX2F(side_horizontal_x, middle_bottom_horizontal_y) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, middle_bottom_horizontal_y) );
 
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 22, EVE_VSIZE - 61) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, EVE_VSIZE - 61) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - side_horizontal_x, middle_bottom_horizontal_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, middle_bottom_horizontal_y) );
 
     //Bottom horizontal lines
-    EVE_cmd_dl_burst( VERTEX2F(186, EVE_VSIZE - 36) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, EVE_VSIZE - 36) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, EVE_VSIZE - middle_horizontal_space_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, EVE_VSIZE - middle_horizontal_space_y) );
 
-    EVE_cmd_dl_burst( VERTEX2F(186, EVE_VSIZE - 72) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, EVE_VSIZE - 72) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, EVE_VSIZE - 2 * middle_horizontal_space_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, EVE_VSIZE - 2 * middle_horizontal_space_y) );
 
-    EVE_cmd_dl_burst( VERTEX2F(186, EVE_VSIZE - 108) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, EVE_VSIZE - 108) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, EVE_VSIZE - 3 * middle_horizontal_space_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, EVE_VSIZE - 3 * middle_horizontal_space_y) );
 
-    EVE_cmd_dl_burst( VERTEX2F(186, EVE_VSIZE - 144) );
-    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - 186, EVE_VSIZE - 144) );
+    EVE_cmd_dl_burst( VERTEX2F(middle_vertical_x, EVE_VSIZE - 4 * middle_horizontal_space_y) );
+    EVE_cmd_dl_burst( VERTEX2F(EVE_HSIZE - middle_vertical_x, EVE_VSIZE - 4 * middle_horizontal_space_y) );
 
     EVE_cmd_dl_burst( DL_END );
 
@@ -111,7 +117,7 @@ void drawMainCommon(){
 //Draw the main data of the screen
 void drawMainData(gameDataContext_t gameContext){
     EVE_start_cmd_burst();
-    EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
+    EVE_cmd_dl_burst(WHITE);
 
     //Speed
     EVE_cmd_text_burst(0, 9, 20, EVE_OPT_CENTERY, "SPEED");
@@ -125,31 +131,31 @@ void drawMainData(gameDataContext_t gameContext){
     EVE_cmd_number_burst( EVE_HSIZE / 2, 76, 31, EVE_OPT_CENTER, gameContext.gear);
 
     //Delta
-    EVE_cmd_dl_burst(COLOR_RGB(200, 0, 200));
+    EVE_cmd_dl_burst(DELTA_COLOR);
     String deltaString = secondsToTime(gameContext.delta);
-    EVE_cmd_text_burst( 186 - 4, 54, 31, EVE_OPT_CENTERY | EVE_OPT_RIGHTX, deltaString.c_str());
+    EVE_cmd_text_burst( middle_vertical_x - 4, 54, 31, EVE_OPT_CENTERY | EVE_OPT_RIGHTX, deltaString.c_str());
 
     //Brake Bias
-    EVE_cmd_dl_burst(COLOR_RGB(150, 120, 10));
+    EVE_cmd_dl_burst(BRAKE_BIAS_COLOR);
     EVE_cmd_text_burst( EVE_HSIZE - 104, 54, 31, EVE_OPT_CENTER, String(gameContext.brakeBias).substring(0, String(gameContext.brakeBias).indexOf('.') + 2).c_str());
 
     //Battery
-    EVE_cmd_dl_burst(COLOR_RGB(0, 255, 0));
+    EVE_cmd_dl_burst(GREEN);
     EVE_cmd_text_burst( EVE_HSIZE / 2, EVE_VSIZE - 90, 30, EVE_OPT_CENTER, String(gameContext.battery).c_str());
-    EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
-    EVE_cmd_text_burst( 186 + 4, EVE_VSIZE - 100, 16, EVE_OPT_CENTERY, "BATT");
+    EVE_cmd_dl_burst(WHITE);
+    EVE_cmd_text_burst( middle_vertical_x + 4, EVE_VSIZE - 100, 16, EVE_OPT_CENTERY, "BATT");
 
     //Last Lap Fuel
-    EVE_cmd_dl_burst(COLOR_RGB(255, 0, 0));
+    EVE_cmd_dl_burst(RED);
     EVE_cmd_text_burst( EVE_HSIZE / 2, EVE_VSIZE - 54, 30, EVE_OPT_CENTER, String(gameContext.lastLapFuel).c_str());
-    EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
-    EVE_cmd_text_burst( 186 + 4, EVE_VSIZE - 64, 16, EVE_OPT_CENTERY, "LL");
+    EVE_cmd_dl_burst(WHITE);
+    EVE_cmd_text_burst( middle_vertical_x + 4, EVE_VSIZE - 64, 16, EVE_OPT_CENTERY, "LL");
 
     //Last Lap Time
-    EVE_cmd_dl_burst(COLOR_RGB(10, 200, 200));
+    EVE_cmd_dl_burst(LAST_LAP_COLOR);
     EVE_cmd_text_burst( EVE_HSIZE - 104, EVE_VSIZE - 30, 30, EVE_OPT_CENTER, String(gameContext.lastLapTime).c_str());
-    EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
-    EVE_cmd_text_burst( EVE_HSIZE - 186 + 2, EVE_VSIZE - 61 + 8, 16, EVE_OPT_CENTERY, "LAST LAP");
+    EVE_cmd_dl_burst(WHITE);
+    EVE_cmd_text_burst( EVE_HSIZE - middle_vertical_x + 2, EVE_VSIZE - 61 + 8, 16, EVE_OPT_CENTERY, "LAST LAP");
 
     //DRS
     //TODO: Draw DRS
@@ -165,19 +171,19 @@ void drawYellowFlag(bool flagBlink){
     EVE_start_cmd_burst();
     EVE_cmd_dl_burst(DL_BEGIN | EVE_RECTS);
     if(flagBlink){
-        EVE_cmd_dl_burst(COLOR_RGB(250, 180, 0));
+        EVE_cmd_dl_burst(YELLOW_FLAG_COLOR);
     }
     else{
-        EVE_cmd_dl_burst(COLOR_RGB(0, 0, 0));
+        EVE_cmd_dl_burst(BLACK);
     }
     EVE_cmd_dl_burst(VERTEX2F(0, 0));
     EVE_cmd_dl_burst(VERTEX2F(EVE_HSIZE, EVE_VSIZE));
     EVE_cmd_dl_burst(DL_END);
      if(flagBlink){
-        EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
+        EVE_cmd_dl_burst(WHITE);
     }
     else{
-        EVE_cmd_dl_burst(COLOR_RGB(250, 180, 0));
+        EVE_cmd_dl_burst(YELLOW_FLAG_COLOR);
     }
     EVE_cmd_text_burst(EVE_HSIZE / 2, EVE_VSIZE / 2, 31, EVE_OPT_CENTER, "Yellow Flag");
     EVE_end_cmd_burst();
@@ -188,19 +194,19 @@ void drawRedFlag(bool flagBlink){
     EVE_start_cmd_burst();
     EVE_cmd_dl_burst(DL_BEGIN | EVE_RECTS);
     if(flagBlink){
-        EVE_cmd_dl_burst(COLOR_RGB(200, 0, 0));
+        EVE_cmd_dl_burst(RED_FLAG_COLOR);
     }
     else{
-        EVE_cmd_dl_burst(COLOR_RGB(0, 0, 0));
+        EVE_cmd_dl_burst(BLACK);
     }
     EVE_cmd_dl_burst(VERTEX2F(0, 0));
     EVE_cmd_dl_burst(VERTEX2F(EVE_HSIZE, EVE_VSIZE));
     EVE_cmd_dl_burst(DL_END);
     if(flagBlink){
-        EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
+        EVE_cmd_dl_burst(WHITE);
     }
     else{
-        EVE_cmd_dl_burst(COLOR_RGB(200, 0, 0));
+        EVE_cmd_dl_burst(RED_FLAG_COLOR);
     }
     EVE_cmd_text_burst(EVE_HSIZE / 2, EVE_VSIZE / 2, 31, EVE_OPT_CENTER, "Red Flag");
     EVE_end_cmd_burst();
@@ -212,40 +218,40 @@ void drawERSMode(gameDataContext_t gameContext){
 
     if(gameContext.ERSMode == 0){
         //Brake temperatures
-        EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
+        EVE_cmd_dl_burst(WHITE);
         EVE_cmd_text_burst( 22 + 4, 85 + 4 + 10, 20, EVE_OPT_CENTERY, String(gameContext.frontLeftBrakeTemp).c_str());
         EVE_cmd_text_burst( 458 - 4, 85 + 4 + 10, 20, EVE_OPT_CENTERY, String(gameContext.frontRightBrakeTemp).c_str());
         EVE_cmd_text_burst( 22 + 4, EVE_VSIZE - 61 - 4 - 10, 20, EVE_OPT_CENTERY, String(gameContext.rearLeftBrakeTemp).c_str());
         EVE_cmd_text_burst( 458 - 4, EVE_VSIZE - 61 - 4 - 10, 20, EVE_OPT_CENTERY, String(gameContext.rearRightBrakeTemp).c_str());
 
         //Fuel target
-        EVE_cmd_dl_burst(COLOR_RGB(255, 0, 0));
+        EVE_cmd_dl_burst(WHITE);
         EVE_cmd_text_burst( EVE_HSIZE / 2, EVE_VSIZE - 36/2, 20, EVE_OPT_CENTER, String(gameContext.fuelTarget).c_str());
-        EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
-        EVE_cmd_text_burst( 186 + 4, EVE_VSIZE - 36 + 4 - 8, 16, EVE_OPT_CENTER, "F");
+        EVE_cmd_dl_burst(WHITE);
+        EVE_cmd_text_burst( middle_vertical_x + 4, EVE_VSIZE - 36 + 4 - 8, 16, EVE_OPT_CENTER, "F");
     }
 
     //Tyre temperatures
     int tyre_temp_x_offset = 20;
     if(gameContext.ERSMode == 0) EVE_cmd_dl_burst(COLOR_RGB(0, 250, 154));
-    else EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
-    EVE_cmd_text_burst( 186 - 4 - tyre_temp_x_offset, 85 + 4 + 10, 20, EVE_OPT_CENTERY, String(gameContext.frontLeftTyreTemp).c_str());
-    EVE_cmd_text_burst( EVE_HSIZE - 186 + 4, 85 + 4 + 10, 20, EVE_OPT_CENTERY, String(gameContext.frontRightTyreTemp).c_str());
-    EVE_cmd_text_burst( 186 - 4 - tyre_temp_x_offset, EVE_VSIZE - 61 - 4 - 10, 20, EVE_OPT_CENTERY, String(gameContext.rearLeftTyreTemp).c_str());
-    EVE_cmd_text_burst( EVE_HSIZE - 186 + 4, EVE_VSIZE - 61 - 4 - 10, 20, EVE_OPT_CENTERY, String(gameContext.rearRightTyreTemp).c_str());
+    else EVE_cmd_dl_burst(WHITE);
+    EVE_cmd_text_burst( middle_vertical_x - 4 - tyre_temp_x_offset, 85 + 4 + 10, 20, EVE_OPT_CENTERY, String(gameContext.frontLeftTyreTemp).c_str());
+    EVE_cmd_text_burst( EVE_HSIZE - middle_vertical_x + 4, 85 + 4 + 10, 20, EVE_OPT_CENTERY, String(gameContext.frontRightTyreTemp).c_str());
+    EVE_cmd_text_burst( middle_vertical_x - 4 - tyre_temp_x_offset, EVE_VSIZE - 61 - 4 - 10, 20, EVE_OPT_CENTERY, String(gameContext.rearLeftTyreTemp).c_str());
+    EVE_cmd_text_burst( EVE_HSIZE - middle_vertical_x + 4, EVE_VSIZE - 61 - 4 - 10, 20, EVE_OPT_CENTERY, String(gameContext.rearRightTyreTemp).c_str());
 
     //ERS mode
     //Draw the background color of the ERS mode
     if(gameContext.ERSMode != 0){
         switch(gameContext.ERSMode){
             case 1:
-                EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
+                EVE_cmd_dl_burst(WHITE);
                 break;
             case 2:
-                EVE_cmd_dl_burst(COLOR_RGB(0, 255, 0));
+                EVE_cmd_dl_burst(GREEN);
                 break;
             case 3:
-                EVE_cmd_dl_burst(COLOR_RGB(0, 0, 255));
+                EVE_cmd_dl_burst(BLUE);
                 break;
             case 4:
             case 5:
@@ -254,15 +260,15 @@ void drawERSMode(gameDataContext_t gameContext){
                 break;
         }
         EVE_cmd_dl(DL_BEGIN | EVE_RECTS);
-        EVE_cmd_dl_burst(VERTEX2F(186 + 1, EVE_VSIZE - 36*4 + 1));
-        EVE_cmd_dl_burst(VERTEX2F(EVE_HSIZE - 186 - 1, EVE_VSIZE - 36*3 - 1));
+        EVE_cmd_dl_burst(VERTEX2F(middle_vertical_x + 1, EVE_VSIZE - 36*4 + 1));
+        EVE_cmd_dl_burst(VERTEX2F(EVE_HSIZE - middle_vertical_x - 1, EVE_VSIZE - 36*3 - 1));
         EVE_cmd_dl_burst(DL_END);
     }
 
     //Draw the text of the ERS mode
-    if(gameContext.ERSMode == 1) EVE_cmd_dl_burst(COLOR_RGB(0, 0, 0));
-    else EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
-    EVE_cmd_text_burst( EVE_HSIZE / 2, EVE_VSIZE - (3*36 + 36/2), 20, EVE_OPT_CENTER, ERSModeText(gameContext.ERSMode).c_str());
+    if(gameContext.ERSMode == 1) EVE_cmd_dl_burst(BLACK);
+    else EVE_cmd_dl_burst(WHITE);
+    EVE_cmd_text_burst( EVE_HSIZE / 2, EVE_VSIZE - (3 * middle_horizontal_space_y + middle_horizontal_space_y / 2), 20, EVE_OPT_CENTER, ERSModeText(gameContext.ERSMode).c_str());
 
     EVE_end_cmd_burst();
 }
@@ -271,7 +277,7 @@ void drawERSMode(gameDataContext_t gameContext){
 void drawFPS(uint32_t deltaTime){
     String refreshDelta = String(deltaTime) + " ms " + String(1000.0/(deltaTime)) + " fps";
     EVE_start_cmd_burst();
-    EVE_cmd_dl_burst(COLOR_RGB(255, 255, 255));
+    EVE_cmd_dl_burst(WHITE);
     EVE_cmd_text_burst(EVE_HSIZE / 2, 15, 22, EVE_OPT_CENTER, refreshDelta.c_str());
     EVE_end_cmd_burst();
 }
@@ -279,9 +285,7 @@ void drawFPS(uint32_t deltaTime){
 //This function convert a float delta time ( negative or positive ) to a string.
 String secondsToTime(float seconds){
     String timeString = "";
-    int minutes = 0;
-    int secondsInt = 0;
-    int milliseconds = 0;
+    int minutes = 0, secondsInt = 0, milliseconds = 0;
     if(seconds < 0){
         timeString += "-";
         seconds *= -1;
